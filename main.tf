ad5390d16a31c12dbc4ae3d50cf014a093363dbc
abhisatbhai <140480508+abhisatbhai@users.noreply.github.com>
2023-10-03 19:52:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,21 +6,21 @@ terraform {
     }
   }
 }
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
 provider "aws" {
-  access_key="var.accesskey"
-  secret_key= "var.secretkey"
-  region     = "us-east-1"
+access_key = var.AWS_ACCESS_KEY
+secret_key = var.AWS_SECRET_KEY
+region = "ap-south-1"
 }
+ 
 resource "aws_instance" "myec2" {
   ami           = "ami-03a6eaae9938c858c"
   instance_type = "t2.micro"
   vpc_security_group_ids=[aws_security_group.web-sg.id]
   key_name="tf-key-pair"
-
  
-
  
-
 tags={
  Name="web-server"
 }
@@ -40,34 +40,4 @@ ingress {
  to_port=80
 protocol="tcp"
 cidr_blocks= ["0.0.0.0/0"]
-}
-
- 
-
- 
-
-ingress {
- from_port=22
- to_port=22
-protocol="tcp"
-cidr_blocks= ["0.0.0.0/0"]
-}
-egress {
- from_port=0
- to_port=0
-protocol="-1"
-cidr_blocks= ["0.0.0.0/0"]
-}
-}
-resource "aws_key_pair" "tf-key-pair-1" {
-key_name = "tf-key-pair"
-public_key = tls_private_key.rsa.public_key_openssh
-}
-resource "tls_private_key" "rsa" {
-algorithm = "RSA"
-rsa_bits  = 4096
-}
-resource "local_file" "tf-key" {
-content  = tls_private_key.rsa.private_key_pem
-filename = "tf-key-pair-1"
 }
